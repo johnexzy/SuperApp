d168db8b2b4eeea43d6de1c9c85edd4fb44a691b
Johnexzy <obajohn75@gmail.com>
2020-05-30 14:23:16 +0100

    Latest Commit

--- src/Logic/MakeImage.php
+++ src/Logic/MakeImage.php
@@ -2,7 +2,7 @@
 namespace Src\Logic;
 class MakeImage {
 
-    public function makeimg(String $base64img){
+    public static function makeimg(String $base64img){
         $path = time().rand(5492, 123456789).'.jpg';
         $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64img));
         if(file_put_contents('../uploads/'.$path, $data)){
--- src/TableGateways/MusicGateWay.php
+++ src/TableGateways/MusicGateWay.php
@@ -14,7 +14,7 @@ namespace Src\TableGateWays;
  * @author ObaJohn
  */
 use Src\Logic\MakeImage;
-class MusicGateWay extends MakeImage{
+class MusicGateWay{
     private $db = null;
     public function __construct($db)
         {       
@@ -24,42 +24,72 @@ class MusicGateWay extends MakeImage{
         
         public function insert(Array $input)
         {
-                $statement = "
-                        INSERT INTO News
+                $statement = "INSERT INTO News
                                 (music_name, music_details, artist, music_key, uploaded_by)
                         VALUES
-                                (:music_name, :music_details, :artist, :music_key, :uploaded_by)
-                ";
-                $statementImage = "
-                        INSERT INTO images
-                                (song_url, song_key)
-                        VALUES
-                                (:song_url, :song_key)
-                ";
+                                (:music_name, :music_details, :artist, :music_key, :uploaded_by)";
                 try {
                         $_key = md5($input['music_key'].rand(123, 2345621));
-                        $statement = $this->db->prepare($statement);
-                        $statementImage = $this->db->prepare($statementImage);
-                        $statement->execute(array(
+                        $query = $this->db->prepare($statement);
+                        $query->execute(array(
                                 'music_name' => $input['music_name'],
                                 'music_details' => $input['music_details'],
                                 'artist' => $input['artist'],
                                 'music_key' => $_key,
                                 'uploaded_by' => $input['author'],
-                                
                         ));
-                        foreach ($input['music_image'] as $image) {
-                                $statementImage->execute(array(
-                                        'image_url' => $this->makeImg($image),
-                                        'image_key' => $_key
-                                ));
-                        }
-                        return $statement->rowCount();
+                        $this->createImage($input['music_images'], $_key);
+                        return $query->rowCount();
                 } catch (\PDOException $e) {
                         exit($e->getMessage());
                 }
         }
-        
+        public function createImage(Array $images, string $key) {
+                $statementImage = "
+                        INSERT INTO images
+                                (image_url, image_key)
+                        VALUES
+                                (:image_url, :image_key)
+                ";
+                try {
+                        
+                        $queryImage = $this->db->prepare($statementImage);
+                        foreach ($images as $image) {
+                                $queryImage->execute(array(
+                                        'image_url' => MakeImage::makeImg($image),
+                                        'image_key' => $key
+                                ));
+                        }
+                        
+                        return $queryImage->rowCount();
+                } catch (\PDOException $e) {
+                        exit($e->getMessage());
+                }
+            
+        }
+        public function createSong(Array $songs, string $key) {
+                $statementSong = "
+                        INSERT INTO songs
+                                (image_url, image_key)
+                        VALUES
+                                (:image_url, :image_key)
+                ";
+                try {
+                        
+                        $queryImage = $this->db->prepare($statementImage);
+                        foreach ($images as $image) {
+                                $queryImage->execute(array(
+                                        'image_url' => MakeImage::makeImg($image),
+                                        'image_key' => $key
+                                ));
+                        }
+                        
+                        return $queryImage->rowCount();
+                } catch (\PDOException $e) {
+                        exit($e->getMessage());
+                }
+            
+        }
         public function update($uid, Array $input)
         {       $statement = "
                         UPDATE `News` 
