6263177f17d9b2c9883a8b4ed23fa519dc70b424
Johnexzy <obajohn75@gmail.com>
2020-06-02 20:14:15 +0100

    committed

--- admin/js/audioController.js
+++ admin/js/audioController.js
@@ -1,7 +1,7 @@
-(function($) {
+(function ($) {
   'use strict';
 
-  $(function() {
+  $(function () {
     //   $("#music_title").on("input", function() {
     //     $(this).css({
     //       borderColor: ($(this).val() == "") ? "red" : "#f3f3f3"
@@ -26,10 +26,10 @@
       Image: [],
       Audio: null
     };
-    $('.openfile').on("click", function() {
+    $('.openfile').on("click", function () {
       $(this).parent().find('.file-upload-default').trigger('click')
     })
-    $('.image-upload').on('change', function(e) {
+    $('.image-upload').on('change', function (e) {
 
       for (let i = 0; i < e.target.files.length; i++) {
         const selectedImg = e.target.files[i];
@@ -40,12 +40,12 @@
           musicFile.Image.push(f.target.result);
           $(".del-thumbnail").show();
           $(".image-list").append(
-              "<li tabindex='0' class='el-upload-list__item is-ready'>" +
-              "<img src='" + f.target.result + "' alt='' class='el-upload-list__item-thumbnail'>" +
+            "<li tabindex='0' class='el-upload-list__item is-ready'>" +
+            "<img src='" + f.target.result + "' alt='' class='el-upload-list__item-thumbnail'>" +
 
-              "</li>"
-            )
-            // alert(formData.postImages);
+            "</li>"
+          )
+          // alert(formData.postImages);
         }
         reader.readAsDataURL(selectedImg);
 
@@ -54,7 +54,7 @@
 
     });
 
-    $('.audio-upload').on('change', function(e) {
+    $('.audio-upload').on('change', function (e) {
       // alert((window.location).substr(0, (window.location).indexOf('8090')))
       let hrefs = new String(window.location);
       hrefs = hrefs.split('8090');
@@ -66,28 +66,28 @@
       reader.onload = f => {
         musicFile.Image.push(f.target.result);
         $(".audio-active").html(
-            "<li class='el-upload-list__item is-ready'>" +
-            "<div class='el-upload-list__item-thumbnail'>"+
-            "<div class='el-upload el-upload-item_song'>"+
-            "<i class='mdi mdi-48px mdi-headphones'></i>"+
-            "</div>"+
-            "<audio src='" + f.target.result + "' class='el-upload-list__item-song' controls></audio>" +
-            "</div>"+
-            "</li>"
-          )
-          // alert(formData.postImages);
+          "<li class='el-upload-list__item is-ready'>" +
+          "<div class='el-upload-list__item-thumbnail'>" +
+          "<div class='el-upload el-upload-item_song'>" +
+          "<i class='mdi mdi-48px mdi-headphones'></i>" +
+          "</div>" +
+          "<audio src='" + f.target.result + "' class='el-upload-list__item-song' controls></audio>" +
+          "</div>" +
+          "</li>"
+        )
+        // alert(formData.postImages);
       }
       reader.readAsDataURL(selectedAudio);
 
 
       // alert($(this).val())
     });
-    $(".del-thumbnail").on("click", function() {
+    $(".del-thumbnail").on("click", function () {
       $(".image-list").html("");
       musicFile.Image = [];
       $(this).hide();
     })
-    $('#handleSubmit').on('click', function() {
+    $('#handleSubmit').on('click', function () {
       let hrefs = new String(window.location);
       hrefs = hrefs.split('8090')
       let music_name = $('#music_title').val();
@@ -95,7 +95,7 @@
       let artist = $('#artist').val();
       let uploaded_by = $('#author').val();
       let fields = [music_name, music_details, artist, uploaded_by]
-        //check for empty fields
+      //check for empty fields
       if (musicFile.Image.length < 1 || musicFile.Audio == null) {
         return alert("no image or Audio selected")
       }
@@ -113,16 +113,16 @@
       formData.append('music_name', music_name)
       formData.append('music_details', music_details)
       formData.append('artist', artist)
-      $.each(musicFile.Image, function(key, image) {
+      $.each(musicFile.Image, function (key, image) {
         formData.append(`music_images[${key}]`, image)
       })
       formData.append('music_file', musicFile.Audio)
       formData.append('author', uploaded_by)
 
       $.ajax({
-        xhr: function() {
+        xhr: function () {
           let xhr = new window.XMLHttpRequest();
-          xhr.upload.addEventListener("progress", function(ext) {
+          xhr.upload.addEventListener("progress", function (ext) {
             if (ext.lengthComputable) {
               let perCentComplete = ((ext.loaded / ext.total) * 100).toFixed();
               $(".progress-bar").width(perCentComplete + '%');
@@ -131,7 +131,7 @@
           }, false)
           return xhr;
         },
-        beforeSend: function() {
+        beforeSend: function () {
           $(".progress-bar").html('0%');
 
           $(".progress-bar").width('0%');
@@ -140,25 +140,37 @@
         type: 'POST',
         data: formData,
         processData: false,
-        contentType: false,
-        success: function(data) {
+        contentType: false
+
+      })
+      .done(function () {
+        $(".status-msg").show()
+        //reset All State to default
+        $("#handleSubmit").html('<i class="mdi mdi-upload btn-icon-prepend"></i>Upload</button>')
+        $('#music_title').val() == "";
+        $('#music_title').val() == "";
+        $('#music_title').val() == "";
+
+        $(".del-thumbnail").click();
+        $('body,html').animate({
+          scrollTop: -1
+        }, 1000);
+        $(".status-msg").slideUp(4000)
+      })
+      .fail(function(err){
+          console.log(err)
           $(".status-msg").show()
-          //reset All State to default
-          $("#handleSubmit").html('<i class="mdi mdi-upload btn-icon-prepend"></i>Upload</button>')
-          $('#music_title').val() == "";
-          $('#music_title').val() == "";
-          $('#music_title').val() == "";
+        //reset All State to default
+        $("#handleSubmit").html('<i class="mdi mdi-upload btn-icon-prepend"></i>Upload</button>')
+        $('#music_title').val() == "";
+        $('#music_title').val() == "";
+        $('#music_title').val() == "";
 
-          $(".del-thumbnail").click();
-          $('body,html').animate({
-            scrollTop: -1
-          }, 1000);
-          $(".status-msg").slideUp(4000)
-        },
-        error: function(err) {
-          alert(err);
-        }
-
+        $(".del-thumbnail").click();
+        $('body,html').animate({
+          scrollTop: -1
+        }, 1000);
+        $(".status-msg").slideUp(4000)
       })
     })
 
