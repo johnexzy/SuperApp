a1156fdefe6158283faaf217da3b4dfad09b4c6e
Johnexzy <obajohn75@gmail.com>
2020-06-16 15:17:09 +0100

    building video gateway

--- src/Logic/MakeFile.php
+++ src/Logic/MakeFile.php
@@ -2,18 +2,25 @@
 namespace Src\Logic;
 class MakeFile {
 
-    // public static function makeimg(String $base64img){
-    //     $start = \strpos($base64img, "/") +1;
-    //     $end = \strpos($base64img, ";");
-    //     $ext = \substr($base64img, $start, $end-$start);
-    //     $path = date('YmdHis',time()).mt_rand().".$ext";
-    //     $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64img));
-    //     if(file_put_contents("../uploads/images/$path", $data)){
-    //         return "uploads/images/$path";
-    //     }
+    /**
+     * Decode base64img string, saves the img and return url
+     * @return String
+     */
+    public static function makeimgfrombase64(String $base64img){
+        $start = \strpos($base64img, "/") +1;
+        $end = \strpos($base64img, ";");
+        $ext = \substr($base64img, $start, $end-$start);
+        $path = date('YmdHis',time()).mt_rand().".$ext";
+        $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $base64img));
+        if(file_put_contents("../uploads/images/$path", $data)){
+            return "uploads/images/$path";
+        }
     
-    
-    // }
+    }
+    /**
+     * Saves uploaded img and return url|filename
+     * @return String
+     */
     public static function makeimg(Array $image){
             $fileExt = pathinfo($image['name'], PATHINFO_EXTENSION);
             $extensions = array('png', 'jpg', 'jpeg', 'webm', 'ico', 'gif');
@@ -30,7 +37,7 @@ class MakeFile {
     }
 
     /**
-     * Saves Uploaded Files and return file name
+     * Saves Uploaded Music File and return file name
      * @return String
      */
     public static function makesong(Array $audio, $name) {
@@ -45,5 +52,21 @@ class MakeFile {
                 }
                 return "uploads/audios/$path";
             }
+    }
+    /**
+     * Saves Uploaded Video File and return url | filename
+     * @return String
+     */
+    public static function makevideo(Array $video, $name)
+    {
+        $fileExt = \pathinfo($video['name'], PATHINFO_EXTENSION);
+        $ext = array('mp4', 'webm', 'mkv', 'mov');
+        if (\in_array($fileExt, $ext)) {
+            $path = str_replace(" ", "-", $name).mt_rand(0, 400).".$fileExt";
+            if (!move_uploaded_file($video['tmp_name'], "../uploads/videos/$path")) {
+                exit;
+            }
+            return "uploads/videos/$path";
+        }
     }
 }
