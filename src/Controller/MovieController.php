1cf8331d5fac887902b7b9d336fc8152b91ad63b
Johnexzy <obajohn75@gmail.com>
2020-06-16 21:15:59 +0100

    Building Viedo Upload Script FrontEnd

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,165 @@
+<?php
+
+/*
+ * Copyright 2020 hp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Src\Controller;
+
+/**
+ * Description of MusicController
+ *
+ * @author Oba John
+ */
+use Src\TableGateWays\MovieGateway;
+
+class MovieController extends MovieGateWay{
+    
+    private $db, $requestMethod, $input, $limit, $popular, $pageNo, $short_url, $id;
+
+    public function __construct($db, $requestMethod, Array $input = null, int $id = null, int $limit = null, int $popular = null, int $pageNo = null, String $short_url = null) {
+        parent::__construct($db);
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+        $this->input = $input;
+        $this->limit = $limit;
+        $this->popular = $popular;
+        $this->pageNo = $pageNo;
+        $this->short_url = $short_url;
+        $this->id = $id;
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            
+            case 'POST':
+                $response = $this->addNewVideoFromRequest();
+                break;
+            case 'GET' :
+                if ($this->short_url !== null) {
+                    $response = $this->getVideoByUrl($this->short_url);
+                }
+                elseif ($this->popular) {
+                    $response = $this->getVideoByPopular($this->popular);
+                }
+                elseif ($this->pageNo) {
+                    $response = $this->getVideoByPage($this->pageNo);
+                }
+                else {
+                    $response = $this->getAllVideos($this->limit);
+                };
+                break;
+            case 'DELETE':
+                if ($this->id) {
+                    $response = $this->deleteMusic($this->id);
+                }
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    /**
+     * Controller Method for Create Requests. 
+     */
+    private function addNewVideoFromRequest() {
+        $result = $this->insert($this->input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+       $response['body'] = \json_encode($result);
+        return $response;
+    }
+    private function getVideoByUrl($short_url)
+    {
+        $result = $this->getByUrl($short_url);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+    private function getAllVideos($limit)
+    {
+        $result = $this->getAll($limit);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+    private function getVideoByPage($pageNo)
+    {
+        $result = $this->getPages($pageNo);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+    private function getVideoByPopular($popularInt)
+    {
+        $result = $this->getPopular($popularInt);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+    private function deleteMusic($id) {
+        $result = $this->find($id);
+        if(!$result){
+            return $this->notFoundResponse();
+        }
+        $result = $this->delete($id);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+    public static function reArrayFiles($file_post) {
+
+        $file_arr = array();
+        if(is_countable($file_post['name'])){
+            $file_count = count($file_post['name']);
+            $file_keys = array_keys($file_post);
+            
+            for ($i=0; $i<$file_count; $i++) {
+                foreach ($file_keys as $key) {
+                    $file_arr[$i][$key] = $file_post[$key][$i];
+                    // $file_arr[$key] = $file_post[$key];
+                }
+            }
+        }
+        else{
+            $file_keys = array_keys($file_post);
+    
+        // for ($i=0; $i<$file_count; $i++) {
+            foreach ($file_keys as $key) {
+                // $file_arr[$i][$key] = $file_post[$key][$i];
+                $file_arr[$key] = $file_post[$key];
+
+            }
+        }
+        
+    
+        return $file_arr;
+        
+        /**
+         * @credit to : https://www.php.net/manual/en/features.file-upload.multiple.php#53240
+         */
+    }
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+    }
