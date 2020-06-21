584bd6859cc169689800eb89bfc4f339fb0ad049
Johnexzy <obajohn75@gmail.com>
2020-06-21 17:15:42 +0100

    Building Admin Login

--- src/TableGateways/UserGateway.php
+++ src/TableGateways/UserGateway.php
@@ -2,5 +2,10 @@
 namespace Src\TableGateways;
 
 class UserGateway{
-    protected $name;
+    protected $username;
+    protected $password;
+
+    public function __construct($username, $password) {
+        $this->username = $username;
+    }
 }
