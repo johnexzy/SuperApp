08a516c363aa51ac4a4289bc673cfc7e4a88f5d7
Johnexzy <obajohn75@gmail.com>
2020-06-22 09:26:11 +0100

    Building Admin Login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,9 +3,16 @@
 namespace Src\Controller;
 
 use Src\TableGateways\UserGateway;
-
+ /**
+  * User Authentication Controller.
+  * @author OBA JOHN
+  */
 
 class UserController extends UserGateway
 {
-    
+    protected $username, $password;
+    public function __construct($username, $password) {
+        $this->username = $username;
+        $this->password = $password;
+    }
 }
