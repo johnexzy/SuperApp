67ba248b0348f8fe2915a21923177dcbb8adad08
Johnexzy <obajohn75@gmail.com>
2020-06-22 09:49:03 +0100

    Building Admin Login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,15 +22,15 @@ class UserController extends UserGateway
      */
     public function proccessRequest()
     {
-        $response = $this->LoginControlller($this->username, $this->password);
+        $response = $this->LoginControlller($this->email, $this->password);
         header($response['status_code_header']);
         if ($response['body']) {
             echo $response['body'];
         }
     }
-    private function LoginControlller($username, $password)
+    private function LoginControlller($email, $password)
     {
-        $result = $this->Login($username, $password);
+        $result = $this->Login($email, $password);
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = \json_encode($result);
         return $response;
