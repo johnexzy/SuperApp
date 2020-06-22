6151c048ec6fe48625f89cd8eab46bc6cbfdbfa2
Johnexzy <obajohn75@gmail.com>
2020-06-22 10:17:28 +0100

    Writing Tests for Login Auth

--- src/TableGateways/UserGateway.php
+++ src/TableGateways/UserGateway.php
@@ -13,14 +13,20 @@ class UserGateway{
     public function Login($email, $password)
     {
         $statement = <<<EOS
-        SELECT * FROM  user WHERE email = '$email';
+        SELECT * FROM  user WHERE email = 'leccelj@gmail.com';
     EOS;
         $statement = $this->db->prepare($statement);
         $statement->execute();
         $result = $statement->fetch(\PDO::FETCH_ASSOC);
-        if (password_verify($password, $result["password"])) {
-            return array("status" => 1, "err" => 0);
+        if ($statement->rowCount()) {
+            if (password_verify($password, $result["password"])) {
+                return array("status" => 1, "err" => 0);
+            }
+            else{
+                return array("status" => 0, "err" => 1);
+            }
         }
+        
         else{
             return array("status" => 0, "err" => 1);
         }
