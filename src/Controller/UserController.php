609ee07ed6e945316737a41d5dbfb4c707eb49ad
Johnexzy <obajohn75@gmail.com>
2020-06-22 09:40:56 +0100

    Building Admin Login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,7 +15,6 @@ class UserController extends UserGateway
         parent::__construct($db);
         $this->username = $username;
         $this->password = $password;
-        $this->db = $db;
     }
     /**
      * Handles the incoming Auth Request, Interact with the Gateway and process output
@@ -35,6 +34,5 @@ class UserController extends UserGateway
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = \json_encode($result);
         return $response;
-        
     }
 }
