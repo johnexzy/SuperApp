2ecbffd6f25bc24c943d1a334270cf99eda8eb2e
Johnexzy <obajohn75@gmail.com>
2020-06-22 09:39:53 +0100

    Building Admin Login

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -99,7 +99,7 @@ class AlbumController extends AlbumGateWay{
     {
         $result = $this->getPages($pageNo);
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
-        $response['body'] = json_encode($result);
+        $response['body'] = \json_encode($result);
         return $response;
     }
     private function getAllAlbums($limit)
