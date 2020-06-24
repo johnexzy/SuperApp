930b72fabb819e6b178cc3f531438574c1c939cd
Johnexzy <obajohn75@gmail.com>
2020-06-24 08:46:01 +0100

    Handling Exceptions

--- admin/home.php
+++ admin/home.php
@@ -182,8 +182,8 @@
           
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <i class="mdi mdi-account menu-icon"></i>
-              <span class="menu-title">Manage Nedia</span>
+              <i class="mdi mdi-silverware menu-icon"></i>
+              <span class="menu-title">Manage Media</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="auth">
@@ -196,12 +196,7 @@
               </ul>
             </div>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="documentation/documentation.php">
-              <i class="mdi mdi-file-document-box-outline menu-icon"></i>
-              <span class="menu-title">Documentation</span>
-            </a>
-          </li>
+  
         </ul>
       </nav>
       <!-- partial -->
@@ -233,22 +228,6 @@
                     <i class="mdi mdi-plus text-muted"></i>
                   </button>
                   <button class="btn btn-primary mt-2 mt-xl-0">Generate report</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row" id="proBanner">
-            <div class="col-md-12 grid-margin">
-              <div class="card bg-gradient-primary border-0">
-                <div class="card-body py-3 px-4 d-flex align-items-center justify-content-between flex-wrap">
-                  <p class="mb-0 text-white font-weight-medium">Like what you see? Checkout our premium version for more.</p>
-                  <div class="d-flex">
-                    <a href="https://github.com/Urbanui/MajesticAdmin-Free-Bootstrap-Admin-Template" target="_blank" class="btn btn-outline-light mr-2">Download free version</a>
-                    <a href="http://www.urbanui.com/majestic-admin-pro/template/" target="_blank" class="btn btn-outline-light mr-2 bg-gradient-danger border-0">Upgrade to Pro</a>
-                    <button id="bannerClose" class="btn border-0 p-0">
-                      <i class="mdi mdi-close text-white"></i>
-                    </button>
-                  </div>
                 </div>
               </div>
             </div>
