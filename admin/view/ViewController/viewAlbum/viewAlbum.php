701e5862e0912b34551ae878c3853fab6c94075a
Johnexzy <obajohn75@gmail.com>
2020-06-24 15:39:14 +0100

    creating Edit Controllers

--- admin/view/ViewController/viewAlbum/viewAlbum.php
+++ admin/view/ViewController/viewAlbum/viewAlbum.php
@@ -2,6 +2,19 @@
 
 namespace ViewController\viewAlbum;
 
+use PDO;
+
+/**
+ * get album shortUrl and display Album
+ */
 class viewAlbum{
+    protected PDO $db;
+    protected $short_url;
+    public function __construct(String $short_url, PDO $db) {
+        $this->short_url = $short_url;
+        $this->db = $db;
+    }
+
+
     
 }
