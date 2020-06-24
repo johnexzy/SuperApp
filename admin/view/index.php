2a349365263a5ba7b97d519c09721cb06ea11d51
Johnexzy <obajohn75@gmail.com>
2020-06-24 13:37:39 +0100

    Buildong Admin edit tools

--- admin/view/index.php
+++ admin/view/index.php
@@ -6,3 +6,16 @@ require '../../bootstrap.php';
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = explode('/', $uri);
+
+if(isset($uri[3])){
+    $group = strip_tags($uri[3]);
+    switch ($$group) {
+        case 'music':
+            # music request handler
+            break;
+        
+        default:
+            # album request handler
+            break;
+    }
+}
