24f6d66e3af593229c4d5c934afa151ddfd98bb8
Johnexzy <obajohn75@gmail.com>
2020-06-24 15:48:55 +0100

    creating Edit Controllers

--- admin/view/ViewController/viewAlbum/viewAlbum.php
+++ admin/view/ViewController/viewAlbum/viewAlbum.php
@@ -3,18 +3,24 @@
 namespace ViewController\viewAlbum;
 
 use PDO;
+use Src\TableGateways\AlbumGateway;
 
 /**
  * get album shortUrl and display Album
  */
-class viewAlbum{
+class viewAlbum extends AlbumGateway{
     protected PDO $db;
     protected $short_url;
     public function __construct(String $short_url, PDO $db) {
+        parent::__construct($db);
         $this->short_url = $short_url;
         $this->db = $db;
     }
 
+    public function getAlbum()
+    {
+        $res = $this->getByUrl($this->short_url);
+    }
 
-    
+
 }
