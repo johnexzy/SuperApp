5db331c5fe22935597781cde51dc14aec76fc852
Johnexzy <obajohn75@gmail.com>
2020-06-24 13:44:19 +0100

    Buildong Admin edit tools

--- admin/view/index.php
+++ admin/view/index.php
@@ -9,11 +9,26 @@ $uri = explode('/', $uri);
 
 if(isset($uri[3])){
     $group = strip_tags($uri[3]);
-    switch ($$group) {
+    switch ($group) {
         case 'music':
             # music request handler
+            echo "music";
             break;
         
+        case 'movie':
+            # movie request handler
+            echo "movie";
+            break;
+
+        case 'album':
+            # album request handler
+            echo "album";
+            break;
+
+        case 'comments':
+            # comment request handler
+            echo "comments";
+            break;
         default:
             # album request handler
             break;
