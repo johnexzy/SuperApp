4ca37b2b7084120b47f4574cb24c9b2171ae8e25
Johnexzy <obajohn75@gmail.com>
2020-06-26 12:09:17 +0100

    modified

--- src/Controller/ViewController/ViewAlbum/ViewAlbum.php
+++ src/Controller/ViewController/ViewAlbum/ViewAlbum.php
@@ -3,47 +3,49 @@
 // namespace ViewController\;
 namespace Src\Controller\ViewController\ViewAlbum;
 
-
 use PDO;
 use Src\Layout\LayoutClass;
 use Src\TableGateways\AlbumGateway;
+
 /**
  * get album shortUrl and display Album
  */
-class ViewAlbum extends AlbumGateway{
-    protected PDO $db;
-    protected $short_url;
-    public function __construct(String $short_url, PDO $db) {
-        parent::__construct($db);
-        $this->short_url = $short_url;
-        $this->db = $db;
-        
-    }
+class ViewAlbum extends AlbumGateway
+{
+ protected $db;
+ protected $_short_url;
+ public function __construct(String $short_url, PDO $db)
+ {
+  parent::__construct($db);
+  $this->short_url = $short_url;
+  $this->db        = $db;
 
-    public function getAlbum($short_url)
-    {
-        $res = $this->findByUrl($short_url);
-        if (!$res) {
-            return null;
-        }
-        $res = $this->getByUrl($short_url);
-        return $res;
-    }
+ }
 
-    public function bodyParser()
-    {
-        $response = $this->getAlbum($this->short_url);
+ public function getAlbum($short_url)
+ {
+  $res = $this->findByUrl($short_url);
+  if (!$res) {
+   return null;
+  }
+  $res = $this->getByUrl($short_url);
+  return $res;
+ }
 
-        if (!$response) {
-            return <<<HTML
+ public function bodyParser()
+ {
+  $response = $this->getAlbum($this->short_url);
+
+  if (!$response) {
+   return <<<HTML
                 <h1>Page not found. 404</h1>
             HTML;
-        }
-        $navBar = LayoutClass::navBar;
-        $sideBar = LayoutClass::sideBar;
-        $footer = LayoutClass::footer;
-        return<<<HTML
-            
+  }
+  $navBar  = LayoutClass::navBar;
+  $sideBar = LayoutClass::sideBar;
+  $footer  = LayoutClass::footer;
+  return <<<HTML
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -81,7 +83,12 @@ class ViewAlbum extends AlbumGateway{
                     <div class="row">
                         <div class="col-12 grid-margin stretch-card">
                             <div class="card">
-                                <div class="card-header">New album</div>
+                                <div class="card-header">
+                                    <h3 class="text-center" style="font-family:monospace">
+                                        <i class="mdi mdi-pencil"></i>
+                                        $response[album_name]
+                                    </h3> 
+                                </div>
                                 <div class="card-body">
                                    <div class="alert alert-success status-msg" style="display: none;">Post Created Successfully</div>
                                    <form class="forms-sample">
@@ -89,13 +96,13 @@ class ViewAlbum extends AlbumGateway{
                                         <label for="postTitle">Title</label>
                                         <font size="0.6" id="titleCap" style="display: block; display: none; text-align: right; float: right;">
                                             Required</font>
-                                        <input type="text" class="form-control" id="album_title" placeholder="Song Title">
+                                        <input type="text" class="form-control" id="album_title" placeholder="Song Title" value="$response[album_name]">
                                     </div>
                                     <div class="form-group">
                                         <label for="postTitle">Artist</label>
                                         <font size="0.6" id="artistCap" style="display: block; display: none; text-align: right; float: right;">
                                             Required</font>
-                                        <input type="text" class="form-control" id="artist" placeholder="Artist">
+                                        <input type="text" class="form-control" id="artist" placeholder="Artist" value="$response[artist]">
                                     </div>
 
 
@@ -103,18 +110,18 @@ class ViewAlbum extends AlbumGateway{
                                         <label for="postBody">About this Song</label>
                                         <font size="0.6" id="AboutCap" style="display: block; display: none; text-align: right; float: right;">
                                             Required</font>
-                                        <textarea class="form-control" id="about_album" rows="16"></textarea>
+                                        <textarea class="form-control" id="about_album" rows="16">$response[album_details]</textarea>
                                     </div>
                                     <div class="form-group">
                                         <label for="postBody">YEAR OF RELEASE</label>
                                         <font size="0.6" id="yearCap" style="display: block; display: none; text-align: right; float: right;">
                                             Required</font>
-                                        <input type="number" class="form-control" id="album_year" placeholder="2020">
+                                        <input type="number" class="form-control" id="album_year" placeholder="2020" value="$response[year]">
                                     </div>
                                     <div class="form-group">
                                         <label for="postTitle">Uploaded by</label>
-                                        
-                                        <input type="text" class="form-control" id="author" value="Leccel" disabled>
+
+                                        <input type="text" class="form-control" id="author" value="$response[uploaded_by]" disabled>
                                     </div>
                                     <div class="form-group">
                                         <ul class="el-upload-list el-upload-list--picture-card image-list">
@@ -131,12 +138,12 @@ class ViewAlbum extends AlbumGateway{
                                     <hr>
                                     <div class="form-group">
                                         <ul class="el-upload-list el-upload-list--picture-card audio-active">
-                                            
+
                                         </ul>
                                         <div class="del-song item-delete" style="display: none;">
                                             <i class="mdi mdi-24px mdi-delete"></i>
                                         </div>
-                                        
+
                                         <input type="file" name="audio[]" class="file-upload-default audio-upload" accept="audio/*" multiple>
                                         <div class="el-upload el-upload--picture-card openfile">
                                             <i class="mdi mdi-48px mdi-music-note-plus"></i>
@@ -145,7 +152,7 @@ class ViewAlbum extends AlbumGateway{
                                     <div class="form-group">
                                         <input type="checkbox" name="popular" class="form-check-success" id="popular"> Add to popular?
                                     </div>
-                                   
+
                                 </form>
                                 </div>
                                 <div class="card-footer">
@@ -183,8 +190,8 @@ class ViewAlbum extends AlbumGateway{
 
 </html>
 
-HTML;        
+HTML;
 
-    }
+ }
 
 }
