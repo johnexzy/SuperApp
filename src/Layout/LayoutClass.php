ff826142729f29244209bbe76e1db3567203a7f6
Johnexzy <obajohn75@gmail.com>
2020-06-26 11:21:54 +0100

    modified

--- src/Layout/LayoutClass.php
+++ src/Layout/LayoutClass.php
@@ -205,7 +205,7 @@ HTML;
     const footer = <<<HTML
         <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a href="https://leccel.net/" target="_blank">Urbanui</a>. All rights reserved.</span>
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a href="https://leccel.net/" target="_blank">Leccel</a>. All rights reserved.</span>
                 <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
             </div>
         </footer>
