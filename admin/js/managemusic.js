03851049a60b12e527b4bcfc63bbcfe880e14d7c
Johnexzy <obajohn75@gmail.com>
2020-06-29 22:13:52 +0100

    modified

--- admin/js/managemusic.js
+++ admin/js/managemusic.js
@@ -3,8 +3,11 @@
 
     $(function(){
         $(".delete").on("click", function(){
+            let music_id = $(this).parent().prop("id")
+            $.ajax({
+                url:`http://127.0.0.1:8090/api/v1/music/delete/${music_id}`
+            })
 
-            
             // $(this).parent().parent().parent().parent().hide()
         })
     })
--- admin/musics.php
+++ admin/musics.php
@@ -73,7 +73,7 @@
                                 </div>
                           </div>
                           <div class="card-footer">
-                              <div class="text-right">
+                              <div class="text-right" id="$music[id]">
                                 <a href="" class="text-decoration-none">
                                     <button type="button" class="btn btn-info btn-rounded btn-icon edit">
                                         <i class="mdi mdi-pencil"></i>
