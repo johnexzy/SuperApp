732f08a6361eb65c5d2df07eec744b37cb88e679
Johnexzy <obajohn75@gmail.com>
2020-06-29 13:33:42 +0100

    modified

--- admin/home.php
+++ admin/home.php
@@ -214,9 +214,9 @@
           </div>
           <div class="row">
             <div class="col-md-7 grid-margin stretch-card">
-              <div class="card">
+              <div class="card"><p class="card-header card-title">Cash deposits</p>
                 <div class="card-body">
-                  <p class="card-title">Cash deposits</p>
+                  
                   <p class="mb-4">To start a blog, think of a topic about and first brainstorm party is ways to write details</p>
                   <div id="cash-deposits-chart-legend" class="d-flex justify-content-center pt-3"></div>
                   <canvas id="cash-deposits-chart"></canvas>
---
+++ admin/musics.php
@@ -0,0 +1,82 @@
+<?php
+  require './session.php';
+  require '../bootstrap.php';
+  use Src\Layout\LayoutClass;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>LecceL::Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.php -->
+    <?= LayoutClass::navBar ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.php -->
+      <?= LayoutClass::sideBar ?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="d-flex justify-content-between flex-wrap">
+                <div class="d-flex align-items-end flex-wrap">
+                  <div class="mr-md-3 mr-xl-5">
+                    <h2>Welcome back, Leccel</h2>
+                    <p class="mb-md-0">Your admin dashboard Add and Manage Media.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-7 grid-margin stretch-card">
+              
+            </div>
+            <div class="col-md-5 grid-margin stretch-card">
+            
+            </div>
+          </div>
+          
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.php -->
+        <?= LayoutClass::footer ?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="js/off-canvas.js"></script>
+  <script src="js/hoverable-collapse.js"></script>
+  <script src="js/template.js"></script>
+  <!-- endinject -->
+  <!-- End custom js for this page-->
+</body>
+
+</html>
+
