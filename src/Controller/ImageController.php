35d7ae20fcc1f764193d821f027153dc83c8c814
Johnexzy <obajohn75@gmail.com>
2020-07-03 16:14:20 +0100

    creating update query

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -21,7 +21,83 @@ use Src\TableGateways\ImageGateway;
       */
      public function processRequest()
      {
-         # code...
-     }
- }
+        switch ($this->requestMethod) {
+            case 'GET':
+                $response = ($this->key)? $this->getKeyedImages($this->key)
+                : $this->addImagesFromGet($this->key);
+                break;
+            case 'POST':
+                $response = $this->addImagesFromRequest();
+                break;
+            case 'PUT':
+                $response = $this->updateImagesFromRequest($this->key);
+                break;
+            case 'DELETE':
+                $response = $this->deleteImages($this->key);
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getKeyedImages($key)
+    {
+        
+    }
+    private function addImagesFromRequest() {
+        
+    }
+    private function addImagesFromGet($input) {
+        
+    }
+    private function updateImagesFromRequest($id) {
+        
+    }
+    private function deleteImages($key) {
+        $result = $this->getPostImages($key);
+        if(!$result){
+            return $this->notFoundResponse();
+        }
+        $this->deleteImages($key);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = NULL;
+        return $response;
+    }
+    private function validateInput($input) {
+        if (! isset($input['comment_key'])) {
+            return false;
+        }
+        if (! isset($input['comment'])) {
+            return false;
+        }
+        return true;
+    }
+    private function validateUpdateInput($input) {
+        if (! isset($input['comment'])) {
+            return false;
+        }
+        return true;
+    }
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = "not found";
+        return $response;
+    }
+}
+
  
--- src/TableGateways/ImageGateway.php
+++ src/TableGateways/ImageGateway.php
@@ -1,20 +1,6 @@
 <?php
 
-/*
- * Copyright 2020 hp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 namespace Src\TableGateways;
 
@@ -24,7 +10,9 @@ namespace Src\TableGateways;
  * @author hp
  */
 use Src\Logic\MakeFile;
-
+/**
+ * Image Processor
+ */
 class ImageGateway {
     //put your code here
     private  $db = null;
@@ -74,6 +62,22 @@ class ImageGateway {
                         exit($e->getMessage());
                 }
                 
+        }
+        public function deleteImages($key)
+        {
+                
+                $statement = "
+                        DELETE FROM images
+                        WHERE image_key = ?;
+                ";
+
+                try {
+                        $statement=$this->db->prepare($statement);
+                        $statement->execute(array($key));
+                        return $statement->rowCount();
+                } catch (\PDOException $e) {
+                        exit($e->getMessage());
+                }
         }
 
 }
