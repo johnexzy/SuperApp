50b95d8492655e3e6398fa09d7c4eac8b73b1461
Johnexzy <obajohn75@gmail.com>
2020-07-03 16:00:14 +0100

    creating update query

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -1,8 +1,27 @@
 <?php
 namespace Src\Controller;
 
+use PDO;
 use Src\TableGateways\ImageGateway;
 
 /**
- * 
+ * Handles Incoming Image Request
  */
+
+ class ImageController extends ImageGateway
+ {
+     protected $key, $requestMethod;
+     public function __construct(PDO $db, String $key, $requestMethod) {
+         parent::__construct($db);
+         $this->key = $key;
+         $this->requestMethod = $requestMethod;
+     }
+     /**
+      * Proccess All request for Images
+      */
+     public function processRequest()
+     {
+         # code...
+     }
+ }
+ 
