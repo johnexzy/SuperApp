5e3966b01acc2adc37ab39165f4550c97623d102
Johnexzy <obajohn75@gmail.com>
2020-08-06 11:20:27 +0100

    new changes

--- modifier.php
+++ modifier.php
@@ -13,16 +13,17 @@ $DBcon = new MySQLi($DBhost,$DBuser,$DBpass,$DBname);
 if ($DBcon->connect_errno) {
     die("ERROR : -> ".$DBcon->connect_error);
 }
-$query = $DBcon->query("SELECT * FROM music");
+$query = $DBcon->query("SELECT * FROM music WHERE (`music_name` LIKE '%God%')");
     while ($row=$query->fetch_assoc()) {
-            $id = $row["id"];
-            $name = str_replace(".", "-", str_replace(" ", "-", $row['music_name']."-".mt_rand()));
-            //$date = date_create();
-            //$setd = date_timestamp_set($date, $stamp);
-            if($DBcon->query("UPDATE `music` SET `short_url` = '$name' WHERE `music`.`id` = '$id'")){
-                echo "succes \n";
-            }
-            else {
-                echo "wrong \n";
-            }
+            // $id = $row["id"];
+            // $name = str_replace(".", "-", str_replace(" ", "-", $row['music_name']."-".mt_rand()));
+            // //$date = date_create();
+            // //$setd = date_timestamp_set($date, $stamp);
+            // if($DBcon->query("UPDATE `music` SET `short_url` = '$name' WHERE `music`.`id` = '$id'")){
+            //     echo "succes \n";
+            // }
+            // else {
+            //     echo "wrong \n";
+            // }
+            echo "$row[id]  $row[music_name] $row[music_details]\n\n\r";
     }
