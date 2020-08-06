98ec197a6ef945988325d72b211a83dc5f9d5c10
Johnexzy <obajohn75@gmail.com>
2020-08-06 11:17:51 +0100

    new changes

---
+++ http/Router.php
@@ -0,0 +1,14 @@
+<?php
+namespace Http;
+
+class Router{
+    private $uri;
+    public function __construct(Array $uri) {
+        $this->uri = $uri;
+    }
+    
+    public static function get()
+    {
+        # code...
+    }
+}
