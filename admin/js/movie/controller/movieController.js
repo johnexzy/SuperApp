54c5761927eb0653512769c60125efeadec4d194
Johnexzy <obajohn75@gmail.com>
2020-08-10 11:39:22 +0100

    committed

--- admin/js/movie/controller/movieController.js
+++ admin/js/movie/controller/movieController.js
@@ -1,8 +1,6 @@
 (function ($) {
     'use strict';
-  
     $(function () {
-
       let videoFile = {
         Image: [],
         Video: null,
@@ -24,7 +22,7 @@
         let video_details = $('#about_video').val();
         let fields = [video_name, video_details]
         if (videoFile.Image.length < 1) {
-          alert("no image or Audio selected")
+          alert("no image selected")
           return false
         }
   
@@ -54,82 +52,80 @@
             $(".del-thumbnail").show();
             $(".image-list").append(
               `<li tabindex='0' class='el-upload-list__item is-ready'>
-              <img src='${f.target.result}' alt='' class='el-upload-list__item-thumbnail'>
+                <img src='${f.target.result}' alt='' class='el-upload-list__item-thumbnail'>
               </li>`
             );
           };
           reader.readAsDataURL(selectedImg);
         });
       });
-  
-        let uploader = new plupload.Uploader({
-          runtimes : 'html5,flash,silverlight,html4',
-          browse_button : document.getElementById('pickfiles'), // you can pass an id...
-          chunk_size : '4mb',
-          unique_names : true,
-          multi_selection: false,
-          url : 'http://127.0.0.1:8090/api/v1/file',
-          flash_swf_url : '../js/Moxie.swf',
-          silverlight_xap_url : '../js/Moxie.xap',
-          max_file_count: 1,
-          multipart_params: movieData,
-          filters : {
-              max_file_size : '1000mb',
-              mime_types: [
-                  {title : "Video files", extensions : "mp4,avi,mkv,mov"}
-              ]
-          },
-          
-          init: {
-              PostInit: function() {
-                  $("#handleSubmit").on('click', function(){
-                    if (checkEmptyField()) {
-                      uploader.start()
-                    }
-                    
-                  })
-              },
 
-              FilesAdded: function(up, files) {
-                  plupload.each(files, function(file) {
-                    $(".video-active").html(
-                        `<li class='el-upload-list__item is-ready'>
-                          <div id="${file.id}" class='el-upload-list__item-thumbnail'>
-                              name: ${(file.name).bold()}
-                              <br>
-                              size: ${(plupload.formatSize(file.size)).bold()}
-                          </div>
-                        </li>`
-                      )
-                  });
-              },
 
-              UploadProgress: function(up, file) {
-                  $(".progress-bar").width(`${file.percent}%`);
-                  $(".progress-bar").html(`${file.percent}%`);
-              },
-              FileUploaded: (up, files, info)=>{
-                  let path = JSON.parse(info.response)
-				          console.log(path.info.path)
-
-                  handleSubmit(path.info.path, path.info.size)
+      let uploader = new plupload.Uploader({
+        runtimes : 'html5,flash,silverlight,html4',
+        browse_button : document.getElementById('pickfiles'), // you can pass an id...
+        chunk_size : '4mb',
+        unique_names : true,
+        multi_selection: false,
+        url : 'http://127.0.0.1:8090/api/v1/file',
+        flash_swf_url : '../js/Moxie.swf',
+        silverlight_xap_url : '../js/Moxie.xap',
+        max_file_count: 1,
+        multipart_params: movieData,
+        filters : {
+            max_file_size : '1000mb',
+            mime_types: [
+                {title : "Video files", extensions : "mp4,avi,mkv,mov"}
+            ]
+        },
+        
+        init: {
+            PostInit: function() {
+                $("#handleSubmit").on('click', function(){
+                  if (checkEmptyField()) {
+                    uploader.start()
+                  }
                   
-              },
-              Error: function(up, err) {
-                  document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
-              }
-          }
-      });
+                })
+            },
 
+            FilesAdded: function(up, files) {
+                plupload.each(files, function(file) {
+                  $(".video-active").html(
+                      `<li class='el-upload-list__item is-ready'>
+                        <div id="${file.id}" class='el-upload-list__item-thumbnail'>
+                            name: ${(file.name).bold()}
+                            <br>
+                            size: ${(plupload.formatSize(file.size)).bold()}
+                        </div>
+                      </li>`
+                    )
+                });
+            },
+
+            UploadProgress: function(up, file) {
+                $(".progress-bar").width(`${file.percent}%`);
+                $(".progress-bar").html(`${file.percent}%`);
+            },
+            FileUploaded: (up, files, info)=>{
+                let path = JSON.parse(info.response)
+                console.log(path.info.path)
+
+                handleSubmit(path.info.path, path.info.size)
+                
+            },
+            Error: function(up, err) {
+                document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
+            }
+        }
+      });
       uploader.init();
-      
       $(".del-thumbnail").on("click", function () {
         $(".image-list").html("");
         videoFile.Image = [];
         $(this).hide();
       })
       function handleSubmit(video_File_url, filebyte) {
-        
         let video_name = $('#video_title').val();
         let video_details = $('#about_video').val();
         let uploaded_by = $('#author').val();
@@ -165,10 +161,10 @@
           $(".status-msg").show()
           //reset All State to default
           $("#handleSubmit").html('<i class="mdi mdi-upload btn-icon-prepend"></i>Upload</button>')
-        //   $('#music_title').val() == "";
-        //   $('#music_title').val() == "";
-        //   $('#music_title').val() == "";
-  
+          //Changes ProgressBar colour on success
+          $(".progress-bar").css({
+            background: "#cccc0c"
+          })
           $(".del-thumbnail").click();
           $('body,html').animate({
             scrollTop: -1
@@ -178,9 +174,7 @@
         .fail(function(err){
           alert("Sorry, Something went wrong \nif problem persist contact developer")
             console.log(err)
-            
         })
       }
-  
     });
   })(jQuery);
