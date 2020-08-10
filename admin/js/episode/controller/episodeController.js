88ada180eb26fc9bac4046cd81715714b6eb7761
Johnexzy <obajohn75@gmail.com>
2020-08-10 11:39:32 +0100

    committed

--- admin/js/episode/controller/episodeController.js
+++ admin/js/episode/controller/episodeController.js
@@ -5,8 +5,41 @@
 
       let videoFile = {
         Image: [],
-        Video: null
+        Video: null,
+        Error: false
       };
+      let movieData = {
+        video_name :'',
+        video_details :'',
+        category :'',
+        popular :'',
+        author:''
+      }
+      /**
+       * Check for Empty fields
+       * @return boolean
+       */
+      function checkEmptyField(){
+        let ep_name = $('#ep_number').val();
+        let ep_details = $('#about_video').val();
+        let fields = [ep_name, ep_details]
+        if (videoFile.Image.length < 1) {
+          alert("no image selected")
+          return false
+        }
+  
+        for (let field = 0; field < fields.length; field++) {
+          if (fields[field] == '') {
+            videoFile.Error = true
+            alert("All fields are required")
+            return false
+          }
+          else {
+            videoFile.Error = false
+          }
+        }
+        return true
+      }
       $('.openfile').on("click", function () {
         $(this).parent().find('.file-upload-default').trigger('click')
         // alert($('input[name=Category]:checked').val())
@@ -21,49 +54,86 @@
             $(".del-thumbnail").show();
             $(".image-list").append(
               `<li tabindex='0' class='el-upload-list__item is-ready'>
-              <img src='${f.target.result}' alt='' class='el-upload-list__item-thumbnail'>
+                <img src='${f.target.result}' alt='' class='el-upload-list__item-thumbnail'>
               </li>`
             );
           };
           reader.readAsDataURL(selectedImg);
         });
       });
-  
-      $('.video-upload').on('change', function (e) {
-        let selectedVideo = e.target.files[0];
-        videoFile.Video = selectedVideo;
-        // console.log(selectedVideo)
-          $(".video-active").html(
-            `<li class='el-upload-list__item is-ready'>
-            <div class='el-upload-list__item-thumbnail'>
-                
-                size: ${(selectedVideo.size/(1024 * 1024)).toFixed(1).bold()}mb
-            </div>
-            </li>`
-          )
+      let uploader = new plupload.Uploader({
+        runtimes : 'html5,flash,silverlight,html4',
+        browse_button : document.getElementById('pickfiles'), // you can pass an id...
+        chunk_size : '4mb',
+        unique_names : true,
+        multi_selection: false,
+        url : 'http://127.0.0.1:8090/api/v1/file',
+        flash_swf_url : '../js/Moxie.swf',
+        silverlight_xap_url : '../js/Moxie.xap',
+        max_file_count: 1,
+        multipart_params: movieData,
+        filters : {
+            max_file_size : '1000mb',
+            mime_types: [
+                {title : "Video files", extensions : "mp4,avi,mkv,mov"}
+            ]
+        },
+        
+        init: {
+            PostInit: function() {
+                $("#handleSubmit").on('click', function(){
+                  if (checkEmptyField()) {
+                    uploader.start()
+                  }
+                  
+                })
+            },
 
+            FilesAdded: function(up, files) {
+                plupload.each(files, function(file) {
+                  $(".video-active").html(
+                      `<li class='el-upload-list__item is-ready'>
+                        <div id="${file.id}" class='el-upload-list__item-thumbnail'>
+                            name: ${(file.name).bold()}
+                            <br>
+                            size: ${(plupload.formatSize(file.size)).bold()}
+                        </div>
+                      </li>`
+                    )
+                });
+            },
+
+            UploadProgress: function(up, file) {
+                $(".progress-bar").width(`${file.percent}%`);
+                $(".progress-bar").html(`${file.percent}%`);
+            },
+            FileUploaded: (up, files, info)=>{
+                let path = JSON.parse(info.response)
+                console.log(path.info.path)
+
+                handleSubmit(path.info.path, path.info.size)
+                
+            },
+            Error: function(up, err) {
+                document.getElementById('console').appendChild(document.createTextNode("\nError #" + err.code + ": " + err.message));
+            }
+        }
       });
+      uploader.init();
+      
       $(".del-thumbnail").on("click", function () {
         $(".image-list").html("");
         videoFile.Image = [];
         $(this).hide();
       })
-      $('#handleSubmit').on('click', function () {
-        
+      function handleSubmit(video_File_url, filebyte) {
         let series_name = $('.series-name').val();
         let ep_name = $('#ep_number').val();
         let ep_details = $('#about_video').val();
         let season_key = $('.key').val()
-        let fields = [ep_details, ep_name]
-        //check for empty fields
-        if (videoFile.Image.length < 1 || videoFile.Video === null) {
-          return alert("no image or Video File selected for this Episode")
-        }
-  
-        for (let field = 0; field < fields.length; field++) {
-          if (fields[field] == '') {
-            return alert("Episode Detail or Number cannot be blank")
-          }
+        videoFile.Video = video_File_url
+        if (!checkEmptyField()) {
+          return false
         }
         $(this).text("Uploading...")
   
@@ -77,26 +147,9 @@
           formData.append(`video_images[${key}]`, image)
         })
         formData.append('video_file', videoFile.Video)
+        formData.append('video_file_byte', filebyte)
   
         $.ajax({
-          xhr: function () {
-            let xhr = new window.XMLHttpRequest();
-            
-            xhr.upload.addEventListener("progress", function (ext) {
-            
-              if (ext.lengthComputable) {
-                let perCentComplete = ((ext.loaded / ext.total) * 100).toFixed();
-                $(".progress-bar").width(perCentComplete + '%');
-                $(".progress-bar").html(perCentComplete + '%');
-              }
-            }, false)
-            return xhr;
-          },
-          beforeSend: function () {
-            $(".progress-bar").html('0%');
-  
-            $(".progress-bar").width('0%');
-          },
           url: "http://127.0.0.1:8090/api/v1/episode",
           type: 'POST',
           data: formData,
@@ -107,10 +160,11 @@
           $(".status-msg").show()
           //reset All State to default
           $("#handleSubmit").html('<i class="mdi mdi-upload btn-icon-prepend"></i>Upload</button>')
-        //   $('#music_title').val() == "";
-        //   $('#music_title').val() == "";
-        //   $('#music_title').val() == "";
-  
+          //Changes ProgressBar colour on success
+          $(".progress-bar").css({
+            background: "#cccc0c"
+          })
+          
           $(".del-thumbnail").click();
           $('body,html').animate({
             scrollTop: -1
@@ -120,9 +174,7 @@
         .fail(function(err){
           alert("Sorry, Something went wrong \nif problem persist contact developer")
             console.log(err)
-            
         })
-      })
-  
+      }
     });
   })(jQuery);
